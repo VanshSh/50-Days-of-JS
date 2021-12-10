47be45f94296b050c824a409717c595c7ca9c6ab
VanshSh <vanshsharma9354@gmail.com>
2021-12-10 23:18:14 +0530

    Doc: Day 13

--- D13/D13spaceAge.js
+++ D13/D13spaceAge.js
@@ -1,55 +1,58 @@
-// const spaceAge = (seconds) => {
-//   const yearsInAllPlanets = {
-//     Mercury: 0,
-//     Venus: 0,
-//     Earth: 0,
-//     Mars: 0,
-//     Jupiter: 0,
-//     Saturn: 0,
-//     Uranus: 0,
-//     Neptune: 0,
-//   }
+// Convert given seconds to space age on all planets of our solar system
 
-//   // Your solution starts here
+console.log("*** Day 13: Space Age ***")
 
-//   const EARTH_YEAR_IN_SECONDS = 31557600
-//   const MERCURY_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 0.2408467
-//   const VENUS_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 0.61519726
-//   const MARS_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 1.8808158
-//   const JUPITER_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 11.862615
-//   const SATURN_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 29.447498
-//   const URANUS_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 84.016846
-//   const NEPTUNE_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 164.79132
+const spaceAge = (seconds) => {
+  const yearsInAllPlanets = {
+    Mercury: 0,
+    Venus: 0,
+    Earth: 0,
+    Mars: 0,
+    Jupiter: 0,
+    Saturn: 0,
+    Uranus: 0,
+    Neptune: 0,
+  }
 
-//   for (let key in yearsInAllPlanets) {
-//     let spaceAgeInYears = (
-//       seconds /
-//       (key === 'Earth'
-//         ? EARTH_YEAR_IN_SECONDS
-//         : key === 'Mercury'
-//         ? MERCURY_YEAR_IN_SECONDS
-//         : key === 'Venus'
-//         ? VENUS_YEAR_IN_SECONDS
-//         : key === 'Mars'
-//         ? MARS_YEAR_IN_SECONDS
-//         : key === 'Jupiter'
-//         ? JUPITER_YEAR_IN_SECONDS
-//         : key === 'Saturn'
-//         ? SATURN_YEAR_IN_SECONDS
-//         : key === 'Uranus'
-//         ? URANUS_YEAR_IN_SECONDS
-//         : key === 'Neptune'
-//         ? NEPTUNE_YEAR_IN_SECONDS
-//         : 0)
-//     ).toFixed(2)
+  // Your solution starts here
 
-//     yearsInAllPlanets[key] = Number(spaceAgeInYears)
-//   }
+  const EARTH_YEAR_IN_SECONDS = 31557600
+  const MERCURY_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 0.2408467
+  const VENUS_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 0.61519726
+  const MARS_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 1.8808158
+  const JUPITER_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 11.862615
+  const SATURN_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 29.447498
+  const URANUS_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 84.016846
+  const NEPTUNE_YEAR_IN_SECONDS = EARTH_YEAR_IN_SECONDS * 164.79132
 
-//   // Your solution ends here
+  for (let key in yearsInAllPlanets) {
+    let spaceAgeInYears = (
+      seconds /
+      (key === 'Earth'
+        ? EARTH_YEAR_IN_SECONDS
+        : key === 'Mercury'
+        ? MERCURY_YEAR_IN_SECONDS
+        : key === 'Venus'
+        ? VENUS_YEAR_IN_SECONDS
+        : key === 'Mars'
+        ? MARS_YEAR_IN_SECONDS
+        : key === 'Jupiter'
+        ? JUPITER_YEAR_IN_SECONDS
+        : key === 'Saturn'
+        ? SATURN_YEAR_IN_SECONDS
+        : key === 'Uranus'
+        ? URANUS_YEAR_IN_SECONDS
+        : key === 'Neptune'
+        ? NEPTUNE_YEAR_IN_SECONDS
+        : 0)
+    ).toFixed(2)
 
-//   return yearsInAllPlanets
-// }
+    yearsInAllPlanets[key] = Number(spaceAgeInYears)
+  }
 
-// console.log(spaceAge(Math.round(Math.random() *
+  // Your solution ends here
 
+  return yearsInAllPlanets
+}
+
+console.log(spaceAge(Math.round(Math.random() * 99999999)))
